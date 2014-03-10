6aaac0a5ea8393cce6739cae51e317aa9c77c670
Robert Rouhani <robert.rouhani@gmail.com>
2014-03-10 18:46:08 -0400

    Copied and formatted exported symbols from libharfbuzz-0.dll Added SharpFont as a submodule Created basic example that gets the version from HarfBuzz.

---
+++ SharpFont.HarfBuzz.Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using SharpFont.HarfBuzz;
+
+namespace SharpFont.HarfBuzz.Example
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Version string: " + HB.VersionString);
+			Version v = HB.Version;
+			Console.WriteLine("Version: " + v.Major + "." + v.Minor + "." + v.Build);
+			Console.WriteLine("VersionCheck: " + HB.VersionCheck(v));
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ SharpFont.HarfBuzz/HB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SharpFont.HarfBuzz
+{
+	public static partial class HB
+	{
+		public static string VersionString
+		{
+			get
+			{
+				return Marshal.PtrToStringAnsi(hb_version_string());
+			}
+		}
+
+		public static Version Version
+		{
+			get
+			{
+				uint major, minor, micro;
+				hb_version(out major, out minor, out micro);
+				return new Version((int)major, (int)minor, (int)micro);
+			}
+		}
+
+		public static bool VersionCheck(Version version)
+		{
+			return VersionCheck(version.Major, version.Minor, version.Build);
+		}
+
+		public static bool VersionCheck(int major, int minor, int micro)
+		{
+			return hb_version_check((uint)major, (uint)minor, (uint)micro);
+		}
+	}
+}
