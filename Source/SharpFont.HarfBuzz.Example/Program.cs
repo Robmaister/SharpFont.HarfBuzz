350114a1d48a474434e0885efeebafb68901ceaa
Robert Rouhani <robert.rouhani@gmail.com>
2015-12-22 13:53:46 -0800

    Updated copyright

--- Source/SharpFont.HarfBuzz.Example/Program.cs
+++ Source/SharpFont.HarfBuzz.Example/Program.cs
@@ -1,9 +1,5 @@
-﻿#region License
-/**
- * Copyright (c) 2014 Robert Rouhani <robert.rouhani@gmail.com> and other contributors (see CONTRIBUTORS file).
- * Licensed under the MIT License - https://raw.github.com/Robmaister/SharpFont.HarfBuzz/master/LICENSE
- */
-#endregion
+﻿//Copyright (c) 2014-2015 Robert Rouhani <robert.rouhani@gmail.com> and other contributors (see CONTRIBUTORS file).
+//Licensed under the MIT License - https://raw.github.com/Robmaister/SharpFont.HarfBuzz/master/LICENSE
 
 using System;
 using System.Drawing;
