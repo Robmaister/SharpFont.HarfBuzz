72d1afe3e04d8276bb3f6e3ab4ecfde6ac204657
Robert Rouhani <robert.rouhani@gmail.com>
2016-01-05 01:32:30 -0800

    Set up NuGet package, incremented version to 0.1.0

--- Source/SharpFont.HarfBuzz/Properties/AssemblyInfo.cs
+++ Source/SharpFont.HarfBuzz/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿#region License
 /**
- * Copyright (c) 2014 Robert Rouhani <robert.rouhani@gmail.com> and other contributors (see CONTRIBUTORS file).
+ * Copyright (c) 2014-2016 Robert Rouhani <robert.rouhani@gmail.com> and other contributors (see CONTRIBUTORS file).
  * Licensed under the MIT License - https://raw.github.com/Robmaister/SharpFont.HarfBuzz/master/LICENSE
  */
 #endregion
@@ -15,9 +15,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SharpFont.HarfBuzz")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Robmaister")]
 [assembly: AssemblyProduct("SharpFont.HarfBuzz")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Robert Rouhani 2014-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
